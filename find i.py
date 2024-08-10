9312d1441afc274617f7e63341e29ff8121c6192
Shinjan sarkar <shinjansarkar268@gmail.com>
2024-08-10 19:08:04 +0530

    Intial commit

--- find i.py
+++ find i.py
@@ -1,5 +1,34 @@
-word=" i am a boy. i go to school. keep ice bag."
+text = "i am a boy.i go to school. keep ice bag."
+updated_text = text.replace(" i", " I")
+print(updated_text)
 
-capital = word.replace(" i ", " I ")
-print(capital)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import re
+
+# text = " I am a boy.I go to school. keep Ice bag."
+
+# # Replace ' I' with ' i' using regex, ensuring 'I' at the start of a sentence isn't changed
+# updated_text = re.sub(r'(?<!\.) I', ' i', text)
+
+# print(updated_text)
