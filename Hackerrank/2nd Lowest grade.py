249f978297660e11beab57546a59b2cd67fb9436
Shinjan sarkar <shinjansarkar268@gmail.com>
2024-08-11 17:43:14 +0530

    Intial commit

---
+++ Hackerrank/2nd Lowest grade.py
@@ -0,0 +1,9 @@
+#nested list 
+
+# python_students = [['Harsh', 39], ['jon', 0 ], ['Tina', 37.2], ['Akriti', 41], ['Harry', 37.21]]
+# after_sort=sorted(python_students ,reverse=True)
+# print(after_sort)
+# print(after_sort[0])
+
+
+
